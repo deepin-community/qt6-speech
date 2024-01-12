cb2a18a79a2f4757c9f5ac5daa6362f0df070ac9
Tianyu Chen <sweetyfish@deepin.org>
2024-01-12 11:08:19 +0800

    feat: update qt6-speech to 6.6.1-2

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/qt6-speech/issues
+# Bug-Submit: https://github.com/<user>/qt6-speech/issues/new
+# Changelog: https://github.com/<user>/qt6-speech/blob/master/CHANGES
+# Documentation: https://github.com/<user>/qt6-speech/wiki
+# Repository-Browse: https://github.com/<user>/qt6-speech
+# Repository: https://github.com/<user>/qt6-speech.git
